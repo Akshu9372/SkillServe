712eb09fe663f4f52b9e00c4813d00604fd8030d
Akshu9372 <akankshasingh9409@gmail.com>
2025-12-14 17:01:04 +0530

    Project setup done

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+app.get("/", (req, res) =>{
+    res.send({msg: "SkillServe API is Running"});
+})
+//connect DB----------------->>>
+connectDB();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running at Port ${process.env.PORT}`);
+})
